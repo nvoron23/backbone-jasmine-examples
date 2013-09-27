366b8f7c69eeafd1b9a95951d88872a946beb55e
Pieter Joost van de Sande <pj@born2code.net>
2013-09-27 14:01:30 +0200

    Remove AppRoutes test for blank hash

--- spec/javascripts/routers/AppRoutes.spec.js
+++ spec/javascripts/routers/AppRoutes.spec.js
@@ -1,5 +1,5 @@
 describe("AppRouter routes", function() {
-  
+
   beforeEach(function() {
     this.router = new AppRouter;
     this.routeSpy = sinon.spy();
@@ -7,20 +7,12 @@ describe("AppRouter routes", function() {
       Backbone.history.start({silent:true, pushState:true});
     } catch(e) {}
   });
-  
+
   it("fires the todo detail route", function() {
     this.router.bind('route:todo', this.routeSpy);
     this.router.navigate("todo/1", true);
     expect(this.routeSpy).toHaveBeenCalledOnce();
     expect(this.routeSpy).toHaveBeenCalledWith("1");
   });
-  
-  it("fires the index route with a blank hash", function() {
-    this.router.bind("route:index", this.routeSpy);
-    this.router.navigate("", true);
-    expect(this.routeSpy).toHaveBeenCalledOnce();
-    expect(this.routeSpy).toHaveBeenCalledWith();
-  });
-  
 
 });
